4cf4baa8706b3ddc7c13d5c545440e163440682f
Mickel26 <mikolajfilka@gmail.com>
2025-06-02 20:29:46 +0200

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#a85cd1',
+        secondary: '#bb82db',
+        dark: {
+          100: '#622a81',
+          200: '#683285',
+        },
+        light: '#d4b2e7',
+      }
+    },
   },
   plugins: [],
 }
