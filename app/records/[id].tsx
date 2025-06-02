5c6b634c507923fe5b818ee53e367b2664f06456
Mickel26 <mikolajfilka@gmail.com>
2025-06-02 21:11:12 +0200

    Added assets and navbar

---
+++ app/records/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RecordDetails = () => {
+  return (
+    <View>
+      <Text>RecordDetails</Text>
+    </View>
+  )
+}
+
+export default RecordDetails
