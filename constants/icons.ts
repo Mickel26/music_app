c13ebfc78744aa00a4569e4857afa8183caf4bc2
Mickel26 <mikolajfilka@gmail.com>
2025-06-02 21:37:53 +0200

    Changed icons names and added donstants

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import saved from "@/assets/icons/bookmarkIcon.png";
+import home from "@/assets/icons/homeIcon.png";
+import profile from "@/assets/icons/profileIcon.png";
+import search from "@/assets/icons/searchIcon.png";
+
+export const icons = {
+    saved,
+    home,
+    profile,
+    search
+}
