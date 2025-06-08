da8c72a1c7c0cf82957378162b18ad808b8cda70
Mickel26 <mikolajfilka@gmail.com>
2025-06-08 20:33:14 +0200

    Added AlbumDetails interface and fetchAlbumDetails function for album data retrieval

--- services/api.ts
+++ services/api.ts
@@ -1,3 +1,5 @@
+import { AlbumDetails } from "@/interfaces/interfaces";
+
 export const DEEZER_CONFIG = {
     BASE_URL: 'https://api.deezer.com',
     headers: {
@@ -21,4 +23,24 @@ export const fetchMusic = async ({ query }: { query: string }) => {
 
     const data = await response.json();
     return data.data;
+}
+
+export const fetchAlbumDetails = async (albumId: string): Promise<AlbumDetails> => {
+    try {
+        const response = await fetch(`${DEEZER_CONFIG.BASE_URL}/album/${albumId}`, {
+            method: 'GET',
+            headers: DEEZER_CONFIG.headers,
+        });
+
+        if(!response.ok) {
+            throw new Error(`Failed to fetch album details: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching album details:', error);
+        throw error;
+    }
 }
